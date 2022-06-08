b0bcc918e38950b44928eec7bb1ef6a47fd6f419
ayslana <ayslanariene@gmail.com>
2022-06-08 10:51:08 -0300

    Initial Commit

---
+++ ninart_appUITests/ninart_appUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ninart_appUITestsLaunchTests.swift
+//  ninart_appUITests
+//
+//  Created by Ayslana Riene on 08/06/22.
+//
+
+import XCTest
+
+class ninart_appUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
