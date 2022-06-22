a4e026ec90fa6322f1c568cc60e3745160f6d61a
BeatrizLdS <beatriz_.silva@hotmail.com>
2022-06-22 10:32:26 -0300

    solve pull problem

--- ninart_app/Controllers/StoryViewController.swift
+++ ninart_app/Controllers/StoryViewController.swift
@@ -7,12 +7,12 @@
 
 import UIKit
 
-class StoryViewController: UIViewController {
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-   
-        // Do any additional setup after loading the view.
-    }
-
-}
+//class ViewController: UIViewController {
+//    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//   
+//        // Do any additional setup after loading the view.
+//    }
+//
+//}
